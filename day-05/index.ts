b3087ee94f37bf4e6a89df5b49a5b0d52b81537a
Jack Thorpe <jackthorpe1@gmail.com>
2024-12-05 20:59:15 +0000

    Part 2

--- day-05/index.ts
+++ day-05/index.ts
@@ -6,6 +6,17 @@ const lines = protocols.split(/\r\n/);
 const rules: Rule[] = [];
 const updates: Update[] = [];
 
+const getDepth = (rule: SortedRule, depth = 0): number[] => {
+  if (!rule.followedBy.length) {
+    return [rule.page];
+  }
+
+  const nextLayerPages = rule.followedBy.map((r) => getDepth(r, depth + 1));
+  nextLayerPages.sort((a, b) => b.length - a.length);
+
+  return [rule.page, ...nextLayerPages[0]];
+};
+
 for (let line of lines) {
   if (line.indexOf("|") > -1) {
     const values = line.split("|").map((x) => +x);
@@ -61,45 +72,42 @@ const part1 = () => {
 };
 
 const part2 = (invalidUpdates: Update[]) => {
-  // a full chain with have the same length as the number of pages
-  // so i suspect that there will only be one full length chain
+  const sortedUpdates = [];
 
   for (let update of invalidUpdates) {
-    const sortedPages = [...update.pages];
-
     let applicableRules = rules.filter(
       (rule) =>
         update.pages.includes(rule.page) &&
         update.pages.includes(rule.precededBy)
     );
 
-    const firstPages = update.pages.filter(
+    const ruleChain: SortedRule[] = [
+      ...applicableRules.map((r, id) => ({ ...r, followedBy: [], id })),
+    ];
+
+    for (let rule of ruleChain) {
+      rule.followedBy = ruleChain.filter((x) => x.precededBy === rule.page);
+    }
+
+    const firstPage = update.pages.find(
       (page) =>
         applicableRules.some((rule) => rule.precededBy === page) &&
         applicableRules.every((rule) => rule.page !== page)
     );
 
-    const lastPages = update.pages.filter(
-      (page) =>
-        applicableRules.some((rule) => rule.page === page) &&
-        applicableRules.every((rule) => rule.precededBy !== page)
-    );
-
-    if (firstPages.length > 1 || lastPages.length > 1) {
-      throw new Error("Unexpected rule chain");
-    }
-
-    const firstPage = firstPages[0];
-    const lastPage = lastPages[1];
-
-    let currentPage = firstPage;
-
-    const firstRule = applicableRules.filter(
+    const firstRules = ruleChain.filter(
       (rule) => rule.precededBy === firstPage
     );
 
-    console.log(firstRule);
+    const chains = firstRules.map(getDepth);
+    chains.sort((a, b) => b.length - a.length);
+
+    const orderedPages = [firstPage, ...chains[0]];
+
+    sortedUpdates.push({ pages: orderedPages });
   }
+
+  return sortedUpdates;
 };
 
 const part1Result = part1();
@@ -112,11 +120,22 @@ console.log(
   )
 );
 
-console.log(part2(part1Result.invalidUpdates));
+console.log(
+  part2(part1Result.invalidUpdates).reduce(
+    (aggregate, update) =>
+      (aggregate += update.pages[Math.floor(update.pages.length / 2)]),
+    0
+  )
+);
 
 type Rule = {
   precededBy: number;
   page: number;
+};
+
+type SortedRule = Rule & {
+  followedBy: SortedRule[];
+  id: number;
 };
 
 type Update = {
