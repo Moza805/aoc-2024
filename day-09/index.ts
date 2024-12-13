8e76e70da8a8522f45bdbf51ffbd4281aaa8b6b1
Jack Thorpe <jackthorpe1@gmail.com>
2024-12-13 09:56:03 +0000

    Part 1

---
+++ day-09/index.ts
@@ -0,0 +1,54 @@
+import { readFileSync } from "fs";
+
+const diskMap = readFileSync("./input.txt", "utf-8");
+
+const expandDiskMap = (diskMap: string): (string | undefined)[] =>
+  diskMap
+    .split("")
+    .map((x) => +x)
+    .reduce((diskContent, block, index) => {
+      const newDiskContent = [...diskContent];
+      const isSpace = index % 2 === 1;
+      const fileIndex = index / 2;
+
+      const newBlocks = new Array(block).fill(isSpace ? undefined : fileIndex);
+
+      newDiskContent.push(...newBlocks);
+      return newDiskContent;
+    }, []);
+
+const defrag = (disk: (string | undefined)[]) => {
+  const dataBlockCount = disk.filter((x) => x !== undefined).length;
+  let defragged = [...disk];
+
+  while (defragged.indexOf(undefined) < dataBlockCount) {
+    const blockToMove = defragged.findLastIndex((x) => x !== undefined);
+    const moveTo = defragged.findIndex((x) => x === undefined);
+
+    defragged[moveTo] = defragged[blockToMove];
+
+    defragged[blockToMove] = undefined;
+  }
+
+  return defragged;
+};
+
+const calculateChecksum = (disk: (string | undefined)[]) =>
+  disk
+    .filter((x) => x !== undefined)
+    .reduce((checksum, block, index) => (checksum += +block * index), 0);
+
+const part1 = (data) => {
+  const diskContent = expandDiskMap(data);
+
+  const defragged = defrag(diskContent);
+
+  const checksum = calculateChecksum(defragged);
+
+  return checksum;
+};
+
+const part2 = (data: string) => {};
+
+console.log(part1(diskMap));
+console.log(part2(diskMap));
